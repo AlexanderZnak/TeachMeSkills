77e1f03ffa7f7ebadeba3aba1f764bc070283dfb
AlexanderZnak <frolovruslan1990@gmail.com>
2020-06-28 13:12:07 +0300

    Help, i can't understand=) if first player wins it's okay, but when second player wins it is not okay, iteration is continued after that you enter numbers again, program is finished. And one of the problem, when you enter numbers has already existed,my program continues working.

--- src/lesson3Task/Game.java
+++ src/lesson3Task/Game.java
@@ -3,60 +3,69 @@ package lesson3Task;
 import java.util.Scanner;
 
 public class Game {
-    static String[][] game;
+    private static String[][] game = new String[3][3];
+    private static Scanner scanner = new Scanner(System.in);
+    private static boolean x = true;
+
     public static void main(String[] args) {
-        String[][] game = new String[3][3];
+        System.out.println("Игра началась!!!");
+        make();
+        while (x) {
+            print();
+            moveFirst();
+            print();
+            moveSecond();
+        }
+    }
+
+    public static void make() {
         for (int i = 0; i < game.length; i++) {
             for (int a = 0; a < game[0].length; a++) {
                 game[i][a] = "-";
             }
         }
-        System.out.println("Игра началась!!!");
-        printFirst();
-        moveFirst();
-        printSecond();
-        moveSecond();
-        print();
-
-    }
-    static void moveFirst() {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        game[x][y] = "Y";
     }
 
-    static void moveSecond() {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        game[x][y] = "Y";
+    public static void moveFirst() {
+        if ((game[0][0].equals("O") & game[0][1].equals("O") & game[0][2].equals("O")) | (game[0][0].equals("O") & game[1][1].equals("O") & game[2][2].equals("O")) | (game[0][2].equals("O") & game[1][1].equals("O") & game[2][0].equals("O")) | (game[0][0].equals("O") & game[1][0].equals("O") & game[2][0].equals("O")) | (game[2][0].equals("O") & game[2][1].equals("O") & game[2][2].equals("O")) | (game[0][2].equals("O") & game[1][2].equals("O") & game[2][2].equals("O")) | (game[0][1].equals("O") & game[1][1].equals("O") & game[2][1].equals("O"))){
+            System.out.println("Игра окончена, Игрок №2 ПОБЕДИЛ!!!");
+            x = false;
+        }
+        else {
+            System.out.println("Ход игрока № 1");
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            if (game[x][y].equals("O") | game[x][y].equals("X")) {
+                System.out.println("Эта клетка занята, у вас проблемы со зрением и восприятием, ход передается вашему оппоненту");
+            }
+            else
+                game[x][y] = "X";
+        }
     }
 
-    static void print() {
+     public static void moveSecond() {
+         if ((game[0][0].equals("X") & game[0][1].equals("X") & game[0][2].equals("X")) | (game[0][0].equals("X") & game[1][1].equals("X") & game[2][2].equals("X")) | (game[0][2].equals("X") & game[1][1].equals("X") & game[2][0].equals("X")) | (game[0][0].equals("X") & game[1][0].equals("X") & game[2][0].equals("X")) | (game[2][0].equals("X") & game[2][1].equals("X") & game[2][2].equals("X")) | (game[0][2].equals("X") & game[1][2].equals("X") & game[2][2].equals("X")) | (game[0][1].equals("X") & game[1][1].equals("X") & game[2][1].equals("X"))){
+             System.out.println("Игра окончена, Игрок №1 ПОБЕДИЛ!!!");
+             x = false;
+         }
+         else {
+             System.out.println("Ход игрока № 2");
+             int x = scanner.nextInt();
+             int y = scanner.nextInt();
+             if (game[x][y].equals("X") | game[x][y].equals("O")) {
+                 System.out.println("Эта клетка занята, у вас проблемы со зрением и восприятием, ход передается вашему оппоненту");
+             }
+             else
+                 game[x][y] = "O";
+         }
+    }
+
+    public static void print() {
         for (int i = 0; i < game.length; i++) {
             for (int a = 0; a < game[0].length; a++) {
                 System.out.print(game[i][a] + " ");
             }
             System.out.println();
-        }
-    }
-    static void printFirst() {
-        System.out.println("Ход игрока №1");
-        for (int i = 0; i < game.length; i++) {
-            for (int a = 0; a < game[0].length; a++) {
-                if (game[i][a] == "X" | game[a][i] == "X" | game[i][i] == "X" | game[i][game.length -1 -i] == "X")
-                    System.out.println("Игрок №1 победил, игра окончена.");
-            }
-        }
-    }
-    static void printSecond() {
-        System.out.println("Ход игрока №2");
-        for (int i = 0; i < game.length; i++) {
-            for (int a = 0; a < game[0].length; a++) {
-                if (game[i][a] == "Y" | game[a][i] == "Y" | game[i][i] == "Y" | game[i][game.length -1 -i] == "Y")
-                    System.out.println("Игрок №2 победил, игра окончена.");
-            }
         }
     }
 }
