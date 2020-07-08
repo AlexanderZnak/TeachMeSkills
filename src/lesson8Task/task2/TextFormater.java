7faff6d6907d2587abc5a9c14cf2b184121b7e11
AlexanderZnak <frolovruslan1990@gmail.com>
2020-07-08 22:53:14 +0300

    Task2 was remade.

--- src/lesson8Task/task2/TextFormater.java
+++ src/lesson8Task/task2/TextFormater.java
@@ -4,23 +4,22 @@ import java.io.*;
 
 public class TextFormater {
     public static void main(String[] args) {
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("src\\lesson8Task\\task2\\example2.txt"))) {
-            String text = "Hello\nWorld\nI\nam\nProgrammer\nLOL";
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("src\\lesson8Task\\task2\\example.txt"))) {
+            String text = "You don't really like it man\nJust\nHello all\nWorld pop\nI yoy\nam mm\nProgrammer Like Boss\nLOL invisible man like you gug";
             writer.write(text);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("src\\lesson8Task\\task2\\copy2.txt")); BufferedReader reader = new BufferedReader(new FileReader("src\\lesson8Task\\task2\\example2.txt"))) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("src\\lesson8Task\\task2\\copyExample.txt")); BufferedReader reader = new BufferedReader(new FileReader("src\\lesson8Task\\task2\\example.txt"))) {
             String s;
             while ((s = reader.readLine())!= null) {
-                if (getPolidrome(s)) {
+                if (getAmount(s) >= 3 & getAmount(s) <= 5) {
                     writer.write(s + "\n");
                 }
-                else if (getAmount(s) >= 3 & getAmount(s) <= 5) {
+                else if (getPolidrome(s)) {
                     writer.write(s + "\n");
                 }
-
             }
         }
         catch (IOException e) {
@@ -29,13 +28,17 @@ public class TextFormater {
     }
 
     public static int getAmount(String s) {
-        return s.length();
+        String[] str = s.split(" ");
+        return str.length;
     }
 
     public static boolean getPolidrome(String s) {
-        if (s.equalsIgnoreCase(new StringBuffer(s).reverse().toString()))
-            return true;
-        else
-            return false;
+        String[] str = s.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equalsIgnoreCase(new StringBuffer(str[i]).reverse().toString())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
